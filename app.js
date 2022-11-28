a02aab60aad4b3b39a4be562f05d08d196290679
SrHatcher <erickomaldonado11@gmail.com>
2022-11-27 23:47:59 -0600

    ultimos cambios

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cassandra = require('cassandra-driver')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
